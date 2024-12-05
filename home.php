50b2bf60fe27f1a21998cd18fcc1facb13937498
anton <antonios.kaharap@gmail.com>
2024-12-05 09:38:54 +0700

    whopsie

---
+++ home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm navbar-secondary bg-secondary sticky-top py-0 mb-3">
+        <nav class="nav navbar-nav">
+            <a class="navbar-brand ms-4" href="index.php"><img src="img/brand1.png" class="" style="width: 182px; height: 29px;" alt=""></a>
+        </nav>
+        <a class="navbar-brand" href="#"><img src="" alt=""></a>
+    </nav>
+    <body>
+
+
+            
+            
+
+        
+</body>
+</html>
